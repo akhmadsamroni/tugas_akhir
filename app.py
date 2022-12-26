ad33e56286149f5e81b56f74581afa5f73b1f344
samroni <akhmadzamroni138@gmail.com>
2022-12-27 00:56:22 +0700

    v.1.1

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ generator = pipeline(
     model='./model',
     tokenizer= './model')
 
-# prompt = token.encode(add_special_tokens = True)
 
 # def process(text_generator, text: str, max_length: int = 100, do_sample: bool = True, top_k: int = 50, top_p: float = 0.95,
 #             temperature: float = 1.0, max_time: float = 120.0, seed=42, repetition_penalty=1.0):
